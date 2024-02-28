989089b0fbf465c09ef101a4b2e5ab834f30576a
sumansingh226 <sumansingh13062000@gmail.com>
2024-02-29 01:55:56 +0530

    types in ts

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,8 @@
-let myName: string = "suman singh"
+const myName: string = "suman singh";
+console.log("myName", myName);
+
+const num: number = 10;
+const isValid: boolean = true;
+enum enumExample {
+    name = "suman"
+}
