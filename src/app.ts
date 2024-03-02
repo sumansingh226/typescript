69b2bc04ff97407bf433ab9e2453f2903f4bcdb8
sumansingh226 <sumansingh13062000@gmail.com>
2024-03-02 22:26:44 +0530

    typez in ts

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,20 @@ console.log("myName", myName);
 
 const num: number = 10;
 const isValid: boolean = true;
-let unDefiendValue: undefined;
+let unDefinedValue: undefined;
+
 enum enumExample {
     name = "suman",
-    profession = "software Engineer"
-    ,
+    profession = "software Engineer",
     address = "Noida UP",
 }
+
+function myFun(): void {
+    // Function body
+}
+
+const arrayExample: number[] = [1, 2, 3, 4, 5];
+const objectExample: { name: string, age: number } = { name: "Alice", age: 30 };
+
+console.log("arrayExample", arrayExample);
+console.log("objectExample", objectExample);
